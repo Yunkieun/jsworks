9c768128c4a913b16dec5d32b84e85c6a211e326
YunKieun <rena0816@naver.com>
2023-03-29 18:11:32 +0900

    js 소스파일

---
+++ ch01/js/add.js
@@ -0,0 +1,11 @@
+
+let n1 = prompt("첫 번째 수 입력: " , "1~50");
+let n2 = prompt("두 번째 수 입력: " , "1~50");
+let sum_v;
+
+sum_v = parseInt(n1) + parseInt(n2);
+
+document.write("두 수의 합 : " + sum_v);
+
+// 소수 자리수 설정 함수 - 객체.tofixed()
+
